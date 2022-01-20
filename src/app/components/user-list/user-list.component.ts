638982375ed24fcf7077a7d8c4c896182049899e
jrsandovalz <jsandoval@waverleysoftware.com>
2022-01-20 12:55:09 -0400

    improvements

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -3,6 +3,9 @@ import { UserService } from 'src/app/services/user.service';
 import { first } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TodoModalComponent } from '../todo-modal/todo-modal.component';
+import { AlertService } from 'src/app/services/alert.service';
+import { MatDialogRef, MatDialog  } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-user-list',
@@ -13,8 +16,12 @@ export class UserListComponent implements OnInit {
   users: any;
   displayModal: Boolean;
   userTodo: any;
+  dialogRef!: MatDialogRef<ConfirmDialogComponent>;
+
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private alertService: AlertService,
+    public dialog: MatDialog
   ) { 
     this.displayModal = false;
   }
@@ -32,13 +39,23 @@ export class UserListComponent implements OnInit {
       });
   }
   deleteUser(id: string, firstName: string, lastName: string) {
-    if(confirm("Are you sure to delete a user "+firstName + " " + lastName)) {
-      this.userService.delete(id)
-      .pipe(first())
-      .subscribe(() => this.listUsers());
-    }
-    
+    this.alertService.clear();
+    this.dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      disableClose: false
+    });
+    this.dialogRef.componentInstance.confirmMessage = "Are you sure to delete a user "+firstName + " " + lastName +"?"
+
+    this.dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.userService.delete(id)
+        .pipe(first())
+        .subscribe(() => this.listUsers());
+        this.alertService.success('User deleted successfully', { keepAfterRouteChange: true });
+      }
+    });
+
   }
+  
   openToDoModal(userId: any) {
     const userModal: any = [];
     if (this.userTodo?.includes(userId)) {
